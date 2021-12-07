34f5d805c0e00cffd8a159fa65a4db2476f373e3
avichal chadha <avichal.chadha@datapeace.in>
2021-12-08 03:33:33 +0530

    Add support for gunicorn server gateway

--- docsumo/api/views.py
+++ docsumo/api/views.py
@@ -13,15 +13,21 @@ blueprint = Blueprint('api', __name__)
 @blueprint.route('/search/text/', methods=('POST',))
 def search_text():
     data = request.get_json()
+
+    # validate user input
     try:
         validated_data = search_text_schema.load(data)
     except ValidationError as e:
         return jsonify(e.normalized_messages()), HTTPStatus.BAD_REQUEST
+
     file_name = validated_data["file_name"]
     position = validated_data["position"]
+
+    # Load csv file in pandas
     file_path = get_csv_file_path_for_name(file_name)
     df = pd.read_csv(file_path)
 
+    # create a new column- `rect_coordinates` which has instance of `Rectangle` dataclass
     df["rect_coordinates"] = df.apply(
         lambda x: Rectangle(
             x1=x["x0"],
@@ -30,6 +36,8 @@ def search_text():
             y2=x["y0"]
         ), axis=1
     )
+
+    # Create `Rectangle` instance of outer rectangle
     outer_rect = Rectangle(
         x1=position[0], y1=position[1], x2=position[2], y2=position[3]
     )
@@ -38,6 +46,8 @@ def search_text():
             outer_rect, x
         )
     )
+
+    # Filter rows by `is_inside_outer_rect` = True
     inside_rect_df = df.loc[df['is_inside_outer_rect'] == True]
     texts = inside_rect_df['Text'].to_list()
     text = ' '.join([str(text) for text in texts])
---
+++ docsumo/wsgi.py
@@ -0,0 +1,3 @@
+from docsumo.app import create_app
+
+app = create_app()
